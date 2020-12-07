0066b2de4e002958b43774811d904665cb427998
saydus <saydus07@gmail.com>
2020-12-07 15:27:45 -0600

    Started doing aoc7

---
+++ cmake-build-debug/CMakeFiles/aoc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aoc.dir/main.cpp.o"
+  "aoc"
+  "aoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+
+
+
+int main() {
+    std::cout << "Solutions to AOC 2020!" << std::endl;
+
+    return 0;
+}
