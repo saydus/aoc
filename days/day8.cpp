9cecc40c8ecdb8f6f04dd58b1a9b1290b9b1cc3f
saydus <saydus07@gmail.com>
2020-12-08 02:22:00 -0600

    sol8 reformat :

---
+++ days/day8.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+const string INPUT_DIRECTORY = "../inputs/";
+
+
+
+int simulate(vector<pair<string, int>> & program){
+    int accum = 0;
+    set<int> visitedLines;
+
+    for (int i = 0; i < program.size(); ++i) {
+        if (visitedLines.find(i) != visitedLines.end()){
+            return 0;
+        }
+
+        visitedLines.insert(i);
+        string command = program[i].first;
+        int argument  = program[i].second;
+
+        if (command == "acc"){
+            accum += argument;
+        }
+        else if (command == "jmp"){
+            i += argument - 1;
+        }
+    }
+
+    return accum;
+}
+
+
+void solution8(){
+    std::ifstream file(INPUT_DIRECTORY + "input8.txt");
+    assert(file.is_open());
+
+    vector<pair<string, int>> program;
+    string line;
+    while(getline(file, line)){
+        program.push_back({line.substr(0, line.find(' ')), stoi(line.substr(line.find(' ') + 1))});
+    }
+
+    for (int i = 0; i < program.size(); ++i) {
+        string command = program[i].first;
+
+        if (program[i].first  == "jmp"){
+            program[i].first = "nop";
+            int accum = simulate(program);
+            if (accum != 0){
+                cout << "Solution: " << accum;
+            }
+            program[i].first = "jmp";
+        }
+
+        else if (program[i].first  == "nop"){
+            program[i].first = "jmp";
+            int accum = simulate(program);
+            if (accum != 0){
+                cout << "Solution: " << accum;
+            }
+            program[i].first = "nop";
+        }
+
+    }
+
+}
+
+
+
